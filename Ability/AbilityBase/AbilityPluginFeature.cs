69a057c58fe924d746cf7409a27bc9d18306eade
vasiliy.alexeev <catavasya@yandex.ru>
2024-08-14 16:49:00 +0300

    - fix SerializeReference field

--- Ability/AbilityBase/AbilityPluginFeature.cs
+++ Ability/AbilityBase/AbilityPluginFeature.cs
@@ -1,7 +1,9 @@
 ﻿namespace UniGame.Ecs.Proto.Ability.SubFeatures
 {
+    using System;
     using LeoEcs.Bootstrap.Runtime;
 
+    [Serializable]
     public abstract class AbilityPluginFeature : EcsFeature
     {
         
